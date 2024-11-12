3ef2a54a8aef34d11567ff8ca6ad9d7ff69b2462
Marko Man <darkobas@gmail.com>
2024-11-13 01:50:31 +0530

    vendor: Add black theme for dark mode [3/3]
    Change-Id: I6bfa5a8d1a1ae31444acbbf60da942c4961d2171
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- config/common.mk
+++ config/common.mk
@@ -253,6 +253,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/no-rro
 
 PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay \
     CustomFontPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
