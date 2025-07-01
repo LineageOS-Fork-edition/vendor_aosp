cb9d0d2693ed5f492b97a959ceb910b0e9620f62
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-07-01 19:31:44 +0000

    vendor: Include certification repo
    Change-Id: I37914eb0488162855ec32d759479ecd886bae05e
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -257,6 +257,9 @@ include vendor/aosp/config/version.mk
 # Signing
 -include vendor/priv-keys/keys.mk
 
+# PlayIntegrity Certification
+-include vendor/certification/config.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
 # Fonts
