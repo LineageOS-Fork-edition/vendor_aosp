4577952799126d8eb36d113e778268c141e1b999
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-08-25 13:15:20 +0000

    vendor: Build QuickTap
    Change-Id: I54b3424182699ae58e298fabc5fdc2b43d17703f
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -192,6 +192,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Quick Tap
+PRODUCT_PACKAGES += \
+    ColumbusService
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
